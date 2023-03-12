0619e85cf2eb9deb7a06d5963ae62674a2982307
Mustafa Taşkın <taskin.mmustafa@gmail.com>
2023-03-12 19:39:19 +0300

    added few more ants

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,43 @@ pub fn setup(mut commands: Commands) {
         hunger: 100,
         strength: 100,
     });
+    commands.spawn(Ant{
+        alive: true,
+        name: "Pippin".to_string(),
+        age: 0,
+        health: 100,
+        role: Role::Male,
+        hunger: 100,
+        strength: 100,
+    });
+    commands.spawn(Ant{
+        alive: true,
+        name: "Ziggy".to_string(),
+        age: 0,
+        health: 100,
+        role: Role::Worker,
+        hunger: 100,
+        strength: 100,
+    });
+    commands.spawn(Ant{
+        alive: true,
+        name: "Biscuit".to_string(),
+        age: 0,
+        health: 100,
+        role: Role::Worker,
+        hunger: 100,
+        strength: 100,
+    });
 }
 
 pub fn print_ants(ant_query: Query<&Ant>) {
     for ant in ant_query.iter(){
-        println!("Name: {},",ant.name);
+        println!("Name: {}\tRole: {:?}",ant.name,ant.role);
     }
     
 }
 
-#[derive(Component)]
+#[derive(Component,Debug)]
 pub enum Role {
     Worker,
     Queen,
