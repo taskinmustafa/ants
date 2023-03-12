3bccdb25409a2794330631e4ec8dd6dfeca13d3b
Mustafa Taşkın <taskin.mmustafa@gmail.com>
2023-03-12 19:28:47 +0300

    Basic ant struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use bevy::prelude::*;
+use bevy::{prelude::*};
 
 fn main() {
     App::new()
-    .add_system(hello_world)
+    .add_startup_system(setup)
     .run();
 }
 
-pub fn hello_world() {
-    println!("Hello, from bevy!");
+pub fn setup(mut commands: Commands) {
+    commands.spawn(Ant{
+        alive: true,
+        name: "Antoinette".to_string(),
+        age: 0,
+        health: 100,
+        role: Role::Queen,
+        hunger: 100,
+        strength: 100,
+    });
+}
+
+#[derive(Component)]
+pub enum Role {
+    Worker,
+    Queen,
+    Male,
+}
+
+#[derive(Component)]
+pub struct Ant{
+    pub alive: bool,
+    pub name: String,
+    pub age: u16,
+    pub health: i16,
+    pub role: Role,
+    pub hunger: i16,
+    pub strength: i16,
 }
