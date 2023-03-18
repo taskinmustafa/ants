16825a580c6d1dbc2744f7e61ba649d00ea56174
Mustafa Taşkın <taskin.mmustafa@gmail.com>
2023-03-18 14:18:09 +0300

    unnecessery function deleted

--- src/main.rs
+++ src/main.rs
@@ -9,45 +9,6 @@ fn main() {
     .run();
 }
 
-pub fn setup(mut commands: Commands) {
-    commands.spawn(Ant{
-        alive: true,
-        name: "Antoinette".to_string(),
-        age: 0,
-        health: 100,
-        role: Role::Queen,
-        hunger: 100,
-        strength: 100,
-    });
-    commands.spawn(Ant{
-        alive: true,
-        name: "Pippin".to_string(),
-        age: 0,
-        health: 100,
-        role: Role::Male,
-        hunger: 100,
-        strength: 100,
-    });
-    commands.spawn(Ant{
-        alive: true,
-        name: "Ziggy".to_string(),
-        age: 0,
-        health: 100,
-        role: Role::Worker,
-        hunger: 100,
-        strength: 100,
-    });
-    commands.spawn(Ant{
-        alive: true,
-        name: "Biscuit".to_string(),
-        age: 0,
-        health: 100,
-        role: Role::Worker,
-        hunger: 100,
-        strength: 100,
-    });
-}
-
 pub fn print_ants(ant_query: Query<&Ant>) {
     for ant in ant_query.iter(){
         println!("Name: {}\tRole: {:?}",ant.name,ant.role);
