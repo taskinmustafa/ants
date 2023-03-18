362a66693e9e3f71bd3928f22b50767423ca80eb
Mustafa Taşkın <taskin.mmustafa@gmail.com>
2023-03-18 13:39:57 +0300

    ant spawning

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use bevy::{prelude::*};
+use bevy::{prelude::*, window::PrimaryWindow, render::view::window};
 
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
-    .add_startup_system(setup)
+    .add_startup_system(spawn_camera)
+    .add_startup_system(spawn_ant)
     .add_system(print_ants)
     .run();
 }
@@ -52,6 +53,49 @@ pub fn print_ants(ant_query: Query<&Ant>) {
         println!("Name: {}\tRole: {:?}",ant.name,ant.role);
     }
     
+}
+
+pub fn spawn_ant(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>
+) {
+    let window = window_query.get_single().unwrap();
+    commands.spawn(
+        (
+            SpriteBundle{
+                transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+                texture: asset_server.load("sprites/ant250x250.png"),
+                ..default()
+            },
+            Ant{
+                alive: true,
+                name: "Antoinette".to_string(),
+                age: 0,
+                health: 100,
+                role: Role::Queen,
+                strength: 100,
+                hunger: 100,
+            },
+
+        )
+    );
+    
+}
+
+pub fn spawn_camera(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(
+        Camera2dBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            ..default()
+        }
+    );
+
 }
 
 #[derive(Component,Debug)]
